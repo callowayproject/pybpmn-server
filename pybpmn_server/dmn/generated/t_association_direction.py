5e7e0f481d82a60e764c666eb78d8f0cca93f265
Corey Oordt <coreyoordt@gmail.com>
2026-01-19 10:52:25 -0600

    Refactor: remove unused generated DMN-related files, update `__init__.py` in `pybpmn_server.dmn.generated` to eliminate unused imports, and clean up redundant code for improved maintainability

--- pybpmn_server/dmn/generated/t_association_direction.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-__NAMESPACE__ = "https://www.omg.org/spec/DMN/20240513/MODEL/"
-
-
-class TAssociationDirection(Enum):
-    NONE = "None"
-    ONE = "One"
-    BOTH = "Both"
--- pybpmn_server/dmn/generated/t_decision_table_orientation.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-__NAMESPACE__ = "https://www.omg.org/spec/DMN/20240513/MODEL/"
-
-
-class TDecisionTableOrientation(Enum):
-    RULE_AS_ROW = "Rule-as-Row"
-    RULE_AS_COLUMN = "Rule-as-Column"
-    CROSS_TABLE = "CrossTable"
