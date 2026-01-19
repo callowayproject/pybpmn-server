597bba8306ed061ab046e6687a643dbd5c5f6d3f
Corey Oordt <coreyoordt@gmail.com>
2026-01-19 12:02:39 -0600

    Refactor: replace `APIData`, `APIModel`, and `APIComponent` with updated `APIDataStore` and `APIModelDataStore`, introduce `get_server` for default server initialization, enhance type annotations, and clean up unused code

--- pybpmn_server/api/api.py
+++ pybpmn_server/api/api.py
@@ -4,9 +4,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
+from pybpmn_server.api.secure_user import SystemUser
+from pybpmn_server.server.bpmn_server import get_server
+
 if TYPE_CHECKING:
     from pybpmn_server.api.secure_user import SecureUser
-    from pybpmn_server.datastore.data_objects import InstanceData, ItemData
+    from pybpmn_server.common.configuration import Settings
+    from pybpmn_server.datastore.data_objects import EventData, InstanceData, ItemData
     from pybpmn_server.elements.interfaces import IDefinition
     from pybpmn_server.engine.interfaces import IExecution
     from pybpmn_server.server.bpmn_server import BPMNServer
@@ -17,70 +21,68 @@ class BPMNAPI:
 
     server: BPMNServer
     engine: APIEngine
-    data: APIData
-    model: APIModel
+    data: APIDataStore
+    model: APIModelDataStore
     default_user: Optional[SecureUser] = None
 
-    def __init__(self, server: BPMNServer):
-        self.server = server
-        self.engine = APIEngine(self)
-        self.data = APIData(self)
-        self.model = APIModel(self)
+    def __init__(self, server: Optional[BPMNServer] = None):
+        self.server = server or get_server()
+        self.config = self.server.configuration
+        self.engine = APIEngine(self.config)
+        self.data = APIDataStore(self.config)
+        self.model = APIModelDataStore(self.config)
 
 
 class APIComponent:
     """Base class for API components."""
 
-    api: BPMNAPI
-
-    def __init__(self, api: BPMNAPI):
-        self.api = api
-
-    @property
-    def server(self) -> BPMNServer:
-        """Return the server instance."""
-        return self.api.server
+    def __init__(self, configuration: Optional[Settings] = None):
+        self.config = configuration
+        self.default_user = SystemUser
 
     def get_user(self, user: Optional[SecureUser]) -> SecureUser:
         """Return the user instance."""
         if user is None:
-            return self.api.default_user
+            return self.default_user
         return user
 
 
 class APIEngine(APIComponent):
     """API component for interacting with the engine."""
 
+    def __init__(self, configuration: Optional[Settings] = None):
+        super().__init__(configuration)
+        self.engine = self.config.engine
+
     async def start(
         self,
         name: str,
+        source: str,
         data: Optional[Dict[str, Any]] = None,
         user: Optional[SecureUser] = None,
-        options: Optional[Dict[str, Any]] = None,
+        start_node_id: Optional[str] = None,
+        parent_item_id: Optional[str] = None,
+        no_wait: bool = False,
     ) -> IExecution:
         """Start a process instance."""
-        if data is None:
-            data = {}
-        if options is None:
-            options = {}
+        data = data or {}
         user = self.get_user(user)
-        return await self.server.engine.start(name, data, options.get("startNodeId"), user.user_name, options)
+        return await self.engine.start(name, source, data, start_node_id, user.user_name, parent_item_id, no_wait)
 
     async def invoke(
         self,
         query: Any,
         data: Optional[Dict[str, Any]] = None,
         user: Optional[SecureUser] = None,
-        options: Optional[Dict[str, Any]] = None,
+        restart: bool = False,
+        recover: bool = False,
+        no_wait: bool = False,
     ) -> IExecution:
         """Invokes engine with qualified query and user data."""
-        if data is None:
-            data = {}
-        if options is None:
-            options = {}
+        data = data or {}
         user = self.get_user(user)
         query = user.qualify_items(query)
-        return await self.server.engine.invoke(query, data, user.user_name, options)
+        return await self.engine.invoke(query, data, user.user_name, restart, recover, no_wait)
 
     async def assign(
         self,
@@ -88,42 +90,33 @@ class APIEngine(APIComponent):
         data: Optional[Dict[str, Any]] = None,
         assignment: Optional[Dict[str, Any]] = None,
         user: Optional[SecureUser] = None,
-        options: Optional[Dict[str, Any]] = None,
     ) -> IExecution:
         """Assigns an activity to a user."""
-        if data is None:
-            data = {}
-        if assignment is None:
-            assignment = {}
-        if options is None:
-            options = {}
+        data = data or {}
+        assignment = assignment or {}
         user = self.get_user(user)
         query = user.qualify_items(query)
-        return await self.server.engine.assign(query, data, assignment, user.user_name, options)
+        return await self.engine.assign(query, data, assignment, user.user_name)
 
     async def throw_message(
         self,
         message_id: str,
         data: Optional[Dict[str, Any]] = None,
         message_matching_key: Any = None,
-        # user: Optional[ISecureUser] = None,
-        # options: Dict[str, Any] = None,
     ) -> IExecution:
         """Throws a message with qualified query and user data."""
         data = data or {}
-        return await self.server.engine.throw_message(message_id, data, message_matching_key)
+        return await self.engine.throw_message(message_id, data, message_matching_key)
 
     async def throw_signal(
         self,
         signal_id: str,
         data: Optional[Dict[str, Any]] = None,
         message_matching_key: Any = None,
-        # user: Optional[ISecureUser] = None,
-        # options: Dict[str, Any] = None,
     ) -> Any:
         """Throws a signal with qualified query and user data."""
         data = data or {}
-        return await self.server.engine.throw_signal(signal_id, data, message_matching_key)
+        return await self.engine.throw_signal(signal_id, data, message_matching_key)
 
     async def start_event(
         self,
@@ -131,29 +124,30 @@ class APIEngine(APIComponent):
         element_id: str,
         data: Optional[Dict[str, Any]] = None,
         user: Optional[SecureUser] = None,
-        options: Optional[Dict[str, Any]] = None,
+        restart: bool = False,
+        recover: bool = False,
     ) -> IExecution:
         """Starts an event with qualified query and user data."""
         data = data or {}
-        options = options or {}
         user = self.get_user(user)
-        return await self.server.engine.start_event(query, element_id, data, user.user_name, options)
+        return await self.engine.start_event(query, element_id, data, user.user_name, restart, recover)
 
-    async def restart(
-        self, item_query: Any, data: Any, user: Optional[SecureUser] = None, options: Optional[Dict[str, Any]] = None
-    ) -> IExecution:
+    async def restart(self, item_query: Any, data: Any, user: Optional[SecureUser] = None) -> IExecution:
         """Restarts an item with qualified query and user data."""
-        options = options or {}
         user = self.get_user(user)
-        return await self.server.engine.restart(item_query, data, user.user_name, options)
+        return await self.engine.restart(item_query, data, user.user_name)
 
     async def upgrade(self, model: str, after_node_ids: List[str]) -> Union[List[str], Dict[str, Any]]:
         """Upgrades a model to the latest version."""
-        return await self.server.engine.upgrade(model, after_node_ids)
+        return await self.engine.upgrade(model, after_node_ids)
 
 
-class APIData(APIComponent):
+class APIDataStore(APIComponent):
     """API component for interacting with data."""
+
+    def __init__(self, configuration: Optional[Settings] = None):
+        super().__init__(configuration)
+        self.data_store = self.config.data_store
 
     async def get_pending_user_tasks(self, query: Dict[str, Any], user: Optional[SecureUser] = None) -> List[ItemData]:
         """Retrieves pending user tasks for the given query and user."""
@@ -165,13 +159,13 @@ class APIData(APIComponent):
         """Finds items based on the given query and user permissions."""
         user = self.get_user(user)
         query = user.qualify_instances(query)
-        return await self.server.data_store.find_items(query)
+        return await self.data_store.find_items(query)
 
     async def find_item(self, query: Any, user: Optional[SecureUser] = None) -> Optional[ItemData]:
         """Finds an item based on the given query and user permissions."""
         user = self.get_user(user)
         query = user.qualify_instances(query)
-        items = await self.server.data_store.find_items(query)
+        items = await self.data_store.find_items(query)
         return items[0] if items else None
 
     async def find_instances(
@@ -180,74 +174,80 @@ class APIData(APIComponent):
         """Finds instances based on the given query and user permissions."""
         user = self.get_user(user)
         query = user.qualify_instances(query)
-        return await self.server.data_store.find_instances(query, options)
+        return await self.data_store.find_instances(query, options)
 
     async def delete_instances(self, query: Any, user: Optional[SecureUser] = None) -> None:
         """Deletes instances based on the given query and user permissions."""
         user = self.get_user(user)
         query = user.qualify_delete_instances(query)
-        return await self.server.data_store.delete_instances(query)
+        return await self.data_store.delete_instances(query)
 
 
-class APIModel(APIComponent):
+class APIModelDataStore(APIComponent):
     """API component for interacting with models."""
 
-    async def get(self, query: Any, user: Optional[SecureUser] = None) -> List[Dict[str, Any]]:
+    def __init__(self, configuration: Optional[Settings] = None):
+        super().__init__(configuration)
+        self.engine = self.config.engine
+        self.model_data_store = self.config.model_data_store
+
+    async def get(self, query: dict[str, Any], user: Optional[SecureUser] = None) -> List[Dict[str, Any]]:
         """Retrieves models based on the given query and user permissions."""
         user = self.get_user(user)
         if user.tenant_id:
             query["owner"] = user.models_owner
-        return await self.server.model_data_store.get(query)
+        return await self.model_data_store.get(query)
 
     async def save(self, name: str, source: str, svg: str, user: Optional[SecureUser] = None) -> bool:
         """Saves a model."""
         user = self.get_user(user)
         if user.can_modify_model(name):
-            return await self.server.model_data_store.save(name, source, svg, user.models_owner)
+            return await self.model_data_store.save(name, source, svg, user.models_owner)
         return False
 
-    async def list(self, query: Any, user: Optional[SecureUser] = None) -> List[str]:
+    async def list(self, query: dict[str, Any], user: Optional[SecureUser] = None) -> List[dict[str, Any]]:
         """Lists models based on the given query and user permissions."""
         user = self.get_user(user)
         if user.tenant_id:
             query["owner"] = user.models_owner
-        return await self.server.model_data_store.get_list(query)
+        return await self.model_data_store.get_list(query)
 
-    async def find_events(self, query: Any, user: Optional[SecureUser] = None) -> List[Any]:
+    async def find_events(self, query: dict[str, Any], user: Optional[SecureUser] = None) -> List[EventData]:
         """Finds events based on the given query and user permissions."""
         user = self.get_user(user)
-        return await self.server.model_data_store.find_events(query, user.models_owner)
+        query = user.qualify_start_events(query)
+        return await self.model_data_store.find_events(query)
 
-    async def find_start_events(self, query: Any, user: Optional[SecureUser] = None) -> List[Any]:
+    async def find_start_events(self, query: dict[str, Any], user: Optional[SecureUser] = None) -> List[EventData]:
         """Finds start events based on the given query and user permissions."""
         user = self.get_user(user)
         query["events.subType"] = None
         query = user.qualify_start_events(query)
-        return await self.server.model_data_store.find_events(query, user.models_owner)
+        return await self.model_data_store.find_events(query)
 
     async def delete(self, name: str, user: Optional[SecureUser] = None) -> None:
         """Deletes a model."""
         user = self.get_user(user)
         if user.can_delete_model(name):
-            return await self.server.model_data_store.delete_model(name, user.models_owner)
+            return await self.model_data_store.delete_model(name)
         return None
 
     async def rename(self, name: str, new_name: str, user: Optional[SecureUser] = None) -> bool:
         """Renames a model."""
         user = self.get_user(user)
         if user.can_modify_model(name):
-            return await self.server.model_data_store.rename_model(name, new_name, user.models_owner)
+            return await self.model_data_store.rename_model(name, new_name)
         return False
 
     async def get_source(self, name: str, user: Optional[SecureUser] = None) -> str:
         """Retrieves the source of a model."""
         user = self.get_user(user)
-        return await self.server.model_data_store.get_source(name, user.models_owner)
+        return await self.model_data_store.get_source(name)
 
     async def load(self, name: str, user: Optional[SecureUser] = None) -> IDefinition:
         """Loads a model."""
         user = self.get_user(user)
-        return await self.server.model_data_store.load(name, user.models_owner)
+        return await self.model_data_store.load(name)
 
     async def export(self, query: Any, folder: str, user: Optional[SecureUser] = None) -> None:
         """Exports models based on the given query and user permissions."""
